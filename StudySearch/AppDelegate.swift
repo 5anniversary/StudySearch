5ff182cdca362f08835b1bd03dd939f06a6c8c91
오준현 <oh.junhyeon95@gmail.com>
2020-03-29 19:03:05 +0900

    [UPDATE] Firebse 연결

--- StudySearch/AppDelegate.swift
+++ StudySearch/AppDelegate.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        debugPrint("start application")
+        
+        FirebaseApp.configure()
         return true
     }
-
-
-
+    
+    
+    
 }
 

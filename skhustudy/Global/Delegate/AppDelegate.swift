c1497619c971a42c95308a13cfdeefd398e74175
Kim HeeJae <iowa329@gmail.com>
2020-04-15 22:38:40 +0900

    [RE-UPDATE] #4 페이스북 회원가입 기능 추가

--- skhustudy/Global/Delegate/AppDelegate.swift
+++ skhustudy/Global/Delegate/AppDelegate.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions:
+            launchOptions
+        )
         
         FirebaseApp.configure()
         return true
@@ -33,6 +40,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            options: options
+        )
+    }
 
 }
 

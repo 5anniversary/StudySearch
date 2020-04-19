4f284b03116ecb69147ab3013966c8b1235e1a79
오준현 <oh.junhyeon95@gmail.com>
2020-04-20 02:38:14 +0900

    update: 카테고리 모델 설정

--- skhustudy/Global/Model/Category.swift
+++ skhustudy/Global/Model/Category.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Category {
+    let name: String
+    let id: Int
+    
+    init?(dictionary: [String:Any]){
+        self.name = dictionary["name"] as? String ?? ""
+        self.id = dictionary["id"] as? Int ?? 0
+    }
+}

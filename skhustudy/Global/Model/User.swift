30ae6c1c9d4a1cdfbaf060bf006aba58a76dac97
오준현 <oh.junhyeon95@gmail.com>
2020-04-20 02:08:56 +0900

    create : UserModel 추가

---
+++ skhustudy/Global/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SKHUStudy
+//
+//  Created by Junhyeon on 2020/04/20.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    <#fields#>
+}

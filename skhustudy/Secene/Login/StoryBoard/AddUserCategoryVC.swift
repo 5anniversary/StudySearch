7cb3a8ca01721cfcb7b24821027ea86dce2d892c
hyunsik won <shik0601@gmail.com>
2020-04-22 01:25:09 +0900

    [UPDATE] #8 기본 틀만 잡았음

--- skhustudy/Secene/Login/StoryBoard/AddUserCategoryVC.swift
+++ skhustudy/Secene/Login/StoryBoard/AddUserCategoryVC.swift
@@ -8,10 +8,30 @@
 
 import UIKit
 
+import Then
+
 class AddUserCategoryVC: UIViewController {
+    
+//    let selectCategoryCollectionView = UICollectionView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        selectCategoryCollectionView.delegate = self
+//        selectCategoryCollectionView.dataSource = self
     }
 
 }
+
+// MARK: - Extension
+
+//extension AddUserCategoryVC: UICollectionViewDelegate, UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Identifier", for: indexPath)
+//
+//        return cell
+//    }
+//}

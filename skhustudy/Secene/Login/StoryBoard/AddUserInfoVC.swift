3e212449db5962d1c9ef2ce46061805e2f5d10e8
hyunsik won <shik0601@gmail.com>
2020-04-21 17:37:10 +0900

    [CREATE] 회원 상세정보 입력 뷰 생성

---
+++ skhustudy/Secene/Login/StoryBoard/AddUserInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  AddUserInfoVC.swift
+//  SKHUStudy
+//
+//  Created by 원현식 on 2020/04/21.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import UIKit
+
+import Then
+
+class AddUserInfoVC: UIViewController {
+    
+    let go
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.backgroundColor = .white
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+    
+}

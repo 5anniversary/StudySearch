8b9d1f27e54c431a1eff22e9608dd569ff8a5adb
hyunsik won <shik0601@gmail.com>
2020-05-13 00:10:24 +0900

    [UPDATE] #5 닉네임 textfield 제거, title 수정, 이메일 저장할 프로퍼티 생성

--- skhustudy/Secene/Login/ViewController/SignUpVC.swift
+++ skhustudy/Secene/Login/ViewController/SignUpVC.swift
@@ -22,7 +22,7 @@ class SignUpVC: UIViewController {
     let titleLabel = UILabel().then {
         $0.font = .boldSystemFont(ofSize: 25)
         $0.sizeToFit()
-        $0.text = "회원가입"
+        $0.text = "비밀번호 설정"
     }
     
     let passwordTextField = UITextField().then {
@@ -38,17 +38,12 @@ class SignUpVC: UIViewController {
         $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
         $0.isSecureTextEntry = true
     }
-    
-    let nicknameTextField = UITextField().then {
-        $0.borderStyle = .none
-        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
-        $0.placeholder = "닉네임"
-    }
-    
+
     let completeButton = UIButton(type: .system).then {
         $0.setTitle("가입 완료", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
+        $0.setTitleColor(.white, for: .normal)
         $0.backgroundColor = UIColor.signatureColor
+        $0.makeRounded(cornerRadius: 5)
         $0.addTarget(self, action: #selector(didTapCompleteButton), for: .touchUpInside)
     }
     
@@ -56,9 +51,7 @@ class SignUpVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         addSubView()
-        
     }
     
     // MARK: - Helper
@@ -67,13 +60,11 @@ class SignUpVC: UIViewController {
         view.endEditing(true)
     }
     
+    var email: String = ""
     @objc func didTapCompleteButton() {
-        // TODO: 입력 필드 모두 입력하고, 입력 형식을 따라야 버튼 활성화 되게
-        guard let email = passwordTextField.text,
-            let pw = passwordVerificationField.text else { return }
+        // TODO: 비밀번호와 비밀번호 확인 입력이 같은지 확인
         
         // TODO: 회원가입 로직
-        
         self.dismiss(animated: true, completion: nil)
         
     }

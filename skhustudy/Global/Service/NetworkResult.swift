e0151100b5b62f3c2af6270549735cc2233c70d0
Kim HeeJae <iowa329@gmail.com>
2020-05-12 16:18:22 +0900

    #35 [CREATE] 서버 연결 코드

---
+++ skhustudy/Global/Service/NetworkResult.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkResult.swift
+//  SKHUStudy
+//
+//  Created by Hee Jae Kim on 2020/05/12.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import Foundation
---
+++ skhustudy/Global/Service/UserService.swift
@@ -0,0 +1,9 @@
+//
+//  UserService.swift
+//  SKHUStudy
+//
+//  Created by Hee Jae Kim on 2020/05/12.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import Foundation

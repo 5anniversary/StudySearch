f001526478b5205801418005b9914b30ae1b4518
hyunsik won <shik0601@gmail.com>
2020-05-13 00:11:00 +0900

    [UPDATE] #34 애니메이션 적용, 자잘한 뷰 수정

--- skhustudy/Secene/Login/ViewController/EmailVerificationVC.swift
+++ skhustudy/Secene/Login/ViewController/EmailVerificationVC.swift
@@ -26,11 +26,12 @@ class EmailVerificationVC: UIViewController {
     }
     
     let sendButton = UIButton(type: .system).then {
-        $0.setTitle("전송", for: .normal)
-        $0.setTitleColor(.white, for: .normal)
-        $0.backgroundColor = .signatureColor
+        $0.setTitle("인증하기", for: .normal)
+        $0.setTitleColor(.signatureColor, for: .normal)
+        $0.backgroundColor = .white
         $0.makeRounded(cornerRadius: 10)
         $0.addTarget(self, action: #selector(didTapSendButton), for: .touchUpInside)
+        
     }
     
     let verificationNumberTextField = UITextField().then {
@@ -38,13 +39,14 @@ class EmailVerificationVC: UIViewController {
         $0.textAlignment = .left
         $0.keyboardType = .numberPad
         $0.borderStyle = .none
+        $0.alpha = 0.0
         $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
     }
     
     let verificationButton = UIButton(type: .system).then {
         $0.setTitle("확인", for: .normal)
-        $0.setTitleColor(.white, for: .normal)
-        $0.backgroundColor = .signatureColor
+        $0.setTitleColor(.signatureColor, for: .normal)
+        $0.backgroundColor = .white
         $0.makeRounded(cornerRadius: 10)
         $0.addTarget(self, action: #selector(didTapVerificationButton), for: .touchUpInside)
         $0.isEnabled = false
@@ -69,15 +71,13 @@ class EmailVerificationVC: UIViewController {
         
         // TODO: 인증번호 이메일로 전송 성공하면
             // 인증번호 로컬 저장(덮어씌우기)
-        UserDefaults.standard.set(verificationNumber, forKey: "emailVerificationID")
-        verificationButton.isEnabled = true
-        verificationButton.alpha = 1.0
-        
+            UserDefaults.standard.set(verificationNumber, forKey: "emailVerificationID")
+            self.verificationAnimate()
         
     }
     
-    @objc func didTapVerificationButton(){
-        // TODO: 인증번호 로컬 저장했던 번호와 비교
+    @objc private func didTapVerificationButton(){
+        // 인증번호 로컬 저장했던 번호와 비교
         if let verificationNumber = UserDefaults.standard.string(forKey: "emailVerificationID"),
             let userInput = verificationNumberTextField.text, userInput != "" {
             // 인증번호가 맞으면 다음 화면으로 이동 + 인증번호 지우기
@@ -85,16 +85,36 @@ class EmailVerificationVC: UIViewController {
                 print("인증 성공")
                 
                 UserDefaults.standard.removeObject(forKey: "emailVerificationID")
+                
                 let sb = UIStoryboard(name: "SignUp", bundle: nil)
                 let vc = sb.instantiateViewController(withIdentifier: "SignUpVC") as! SignUpVC
+                vc.email = emailTextField.text!
                 self.navigationController?.pushViewController(vc, animated: true)
                 
             } else {
                 print("인증 실패")
                 self.simpleAlert(title: "인증 실패", message: "인증번호를 다시 확인해주세요.")
-                
             }
         }
     }
     
+}
+
+extension EmailVerificationVC {
+    private func verificationAnimate(){
+        UIView.animate(
+            withDuration: 1.2,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 1,
+            options: [.curveEaseIn],
+            animations: {
+                self.verificationNumberTextField.alpha = 1.0
+                self.verificationNumberTextField.transform = CGAffineTransform(translationX: 0, y: 5)
+                self.verificationButton.alpha = 1.0
+                self.verificationButton.isEnabled = true
+                self.verificationButton.transform = CGAffineTransform(translationX: 0, y: 5)
+        })
+        
+    }
 }

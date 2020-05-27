9a879d7d7965c2829c3a2e521efb819319dd3c3e
hyunsik won <shik0601@gmail.com>
2020-05-27 15:20:02 +0900

    [UPDATE] #45 채팅 UI 구현 완료

---
+++ skhustudy/Secene/Chat/ViewController/ChatListVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChatListVC.swift
+//  SKHUStudy
+//
+//  Created by 원현식 on 2020/05/27.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import UIKit
+
+class ChatListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

4523aa403c70e037c706bbbac75b76ca2e617406
Kim HeeJae <iowa329@gmail.com>
2020-06-07 17:28:02 +0900

    수정

--- AddStudyVC.swift
+++ AddStudyVC.swift
@@ -10,13 +10,13 @@ import UIKit
 
 import SwiftKeychainWrapper
 
-class AddStudyVC: UIViewController {
+class AddStudyVC: UITableViewController {
     
     // MARK: - UI components
     
     let scrollView = UIScrollView()
     let containerView = UIView()
-    
+
     let studyImageView = UIImageView().then {
         $0.image = UIImage(systemName: "camera")
         $0.contentMode = .scaleAspectFit
@@ -62,25 +62,25 @@ class AddStudyVC: UIViewController {
     
     
     
-//    let category = ["카테고리","IT", "영어", "수학", "취업"]
-//
-//    @IBOutlet var studyNameTextField: UITextField!
-//    @IBOutlet var categoryTextField: UITextField!
-//    @IBOutlet var explanationTextView: UITextView!
-//    @IBOutlet var startDateTextField: UITextField!
-//    @IBOutlet var endDateTextField: UITextField!
-//    @IBOutlet var headCountTextField: UITextField!
-//    @IBOutlet var locationTextField: UITextField!
-//    @IBOutlet var penaltyTextField: UITextField!
-//    @IBOutlet var penaltyButton: UIButton!
-//
-//    let datePicker = UIDatePicker()
+    let category = ["카테고리","IT", "영어", "수학", "취업"]
+
+    @IBOutlet var studyNameTextField: UITextField!
+    @IBOutlet var categoryTextField: UITextField!
+    @IBOutlet var explanationTextView: UITextView!
+    @IBOutlet var startDateTextField: UITextField!
+    @IBOutlet var endDateTextField: UITextField!
+    @IBOutlet var headCountTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var penaltyTextField: UITextField!
+    @IBOutlet var penaltyButton: UIButton!
+
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addSubView()
-//        createPickerView()
+//        addSubView()
+        createPickerView()
         
         title = "스터디"
 //        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .done, target: self, action: #selector(didTapCancelButton))
@@ -88,44 +88,44 @@ class AddStudyVC: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "생성", style: .done, target: self, action: #selector(didTapCreateStudyButton))
     }
     
-//    private func createPickerView() {
-//        // category picker
-//        let categoryPickerView = UIPickerView()
-//        categoryPickerView.delegate = self
-//        categoryPickerView.dataSource = self
-//        categoryTextField.inputView = categoryPickerView
-//
-//        // date picker
-//        datePicker.datePickerMode = .date
-//        startDateTextField.inputView = datePicker
-//        endDateTextField.inputView = datePicker
-//
-//        // 공통 툴바
-//        let toolbar = UIToolbar()
-//        toolbar.sizeToFit()
-//        toolbar.tintColor = .signatureColor
-//        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissPickerView))
-//        toolbar.setItems([doneButton], animated: false)
-//        toolbar.isUserInteractionEnabled = true
-//        startDateTextField.inputAccessoryView = toolbar
-//        endDateTextField.inputAccessoryView = toolbar
-//        categoryTextField.inputAccessoryView = toolbar
-//    }
-//
-//    @objc func dismissPickerView() {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateStyle = .full
-//        let date = dateFormatter.string(from: datePicker.date)
-//
-//        if startDateTextField.isEditing {
-//            startDateTextField.text = date
-//
-//        } else if endDateTextField.isEditing {
-//            endDateTextField.text = date
-//        }
-//
-//        view.endEditing(true)
-//    }
+    private func createPickerView() {
+        // category picker
+        let categoryPickerView = UIPickerView()
+        categoryPickerView.delegate = self
+        categoryPickerView.dataSource = self
+        categoryTextField.inputView = categoryPickerView
+
+        // date picker
+        datePicker.datePickerMode = .date
+        startDateTextField.inputView = datePicker
+        endDateTextField.inputView = datePicker
+
+        // 공통 툴바
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        toolbar.tintColor = .signatureColor
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissPickerView))
+        toolbar.setItems([doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
+        startDateTextField.inputAccessoryView = toolbar
+        endDateTextField.inputAccessoryView = toolbar
+        categoryTextField.inputAccessoryView = toolbar
+    }
+
+    @objc func dismissPickerView() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        let date = dateFormatter.string(from: datePicker.date)
+
+        if startDateTextField.isEditing {
+            startDateTextField.text = date
+
+        } else if endDateTextField.isEditing {
+            endDateTextField.text = date
+        }
+
+        view.endEditing(true)
+    }
     
     @objc private func didTapCancelButton() {
         self.dismiss(animated: true, completion: nil)
@@ -135,44 +135,44 @@ class AddStudyVC: UIViewController {
 //        self.createStudyService()
     }
    
-//    @IBAction func didTapPenaltyButton(_ sender: Any) {
-//        if penaltyTextField.isEnabled  {
-//            penaltyButton.setTitle("", for: .normal)
-//            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
-//            penaltyTextField.text = ""
-//        } else {
-//            penaltyButton.setTitle("✓", for: .normal)
-//            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
-//        }
-//    }
+    @IBAction func didTapPenaltyButton(_ sender: Any) {
+        if penaltyTextField.isEnabled  {
+            penaltyButton.setTitle("", for: .normal)
+            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
+            penaltyTextField.text = ""
+        } else {
+            penaltyButton.setTitle("✓", for: .normal)
+            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
+        }
+    }
     
     
 }
 
-//extension AddStudyVC: UIPickerViewDelegate {}
-//
-//extension AddStudyVC: UIPickerViewDataSource {
-//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return category.count
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//
-//        return category[row]
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        if category[row] == "카테고리" {
-//            return
-//        }
-//        categoryTextField.text = category[row]
-//    }
-//
-//}
+extension AddStudyVC: UIPickerViewDelegate {}
+
+extension AddStudyVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return category.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        return category[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if category[row] == "카테고리" {
+            return
+        }
+        categoryTextField.text = category[row]
+    }
+
+}
 
 // MARK: - 사용자 정보 서버 연결
 

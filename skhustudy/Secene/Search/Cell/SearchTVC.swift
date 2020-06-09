5dd50b996520f84c493e855763c1168bd73c3b94
Junhyeon <oh.junhyeon95@gmail.com>
2020-06-09 14:26:46 +0900

    create: SearchView
    Add Search View

---
+++ skhustudy/Secene/Search/Cell/SearchTVC.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTVC.swift
+//  SKHUStudy
+//
+//  Created by Junhyeon on 2020/06/09.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import UIKit
+
+class SearchTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

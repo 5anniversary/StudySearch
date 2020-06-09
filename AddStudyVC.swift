f5dfced89729659bc211e89e2871211df09c8b4e
Kim HeeJae <iowa329@gmail.com>
2020-06-09 10:54:38 +0900

    #61 스터디 생성 새롭게 작성 (서버 연결 작업 필요)

--- AddStudyVC.swift
+++ AddStudyVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 import SwiftKeychainWrapper
 
-class AddStudyVC: UITableViewController {
+class AddStudyVC: UIViewController {
     
     // MARK: - UI components
     
@@ -30,9 +30,140 @@ class AddStudyVC: UITableViewController {
         $0.contentMode = .scaleAspectFill
     }
     
+    let studyTitleLabel = UILabel().then {
+        $0.text = "스터디 제목"
+        $0.sizeToFit()
+        $0.font = Font.studyTitleLabel
+    }
+    let studyTitleTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+//        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let categoryLabel = UILabel().then {
+            $0.text = "카테고리"
+            $0.sizeToFit()
+            $0.font = Font.studyContentsLabel
+    }
+    let categoryTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let introduceLabel = UILabel().then {
+        $0.text = "소개"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let introduceTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let placeLabel = UILabel().then {
+        $0.text = "장소"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let placeTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+
+    let penaltyLabel = UILabel().then {
+        $0.text = "스터디 벌금"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let penaltyYesButton = UIButton().then {
+        $0.setTitle("있음", for: .normal)
+        $0.makeRounded(cornerRadius: 17)
+        $0.tintColor = .white
+        $0.backgroundColor = .signatureColor
+        $0.addTarget(self, action: #selector(didTapPenaltyYesButton), for: .touchUpInside)
+    }
+    let penaltyNoButton = UIButton().then {
+        $0.setTitle("없음", for: .normal)
+        $0.makeRounded(cornerRadius: 17)
+        $0.tintColor = .white
+        $0.backgroundColor = .signatureColor
+        $0.addTarget(self, action: #selector(didTapPenaltyNoButton), for: .touchUpInside)
+    }
+    
+    let penaltyView = UIView()
+    let penaltyAttendanceLabel = UILabel().then {
+        $0.text = "출석 벌금"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let penaltyAttendanceTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let penaltyLateLabel = UILabel().then {
+        $0.text = "지각 벌금"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let penaltyLateTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let termLabel = UILabel().then {
+        $0.text = "모집 기한"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let termYesButton = UIButton().then {
+        $0.setTitle("있음", for: .normal)
+        $0.makeRounded(cornerRadius: 17)
+        $0.tintColor = .white
+        $0.backgroundColor = .signatureColor
+        $0.addTarget(self, action: #selector(didTapTermYesButton), for: .touchUpInside)
+    }
+    let termNoButton = UIButton().then {
+        $0.setTitle("없음", for: .normal)
+        $0.makeRounded(cornerRadius: 17)
+        $0.tintColor = .white
+        $0.backgroundColor = .signatureColor
+        $0.addTarget(self, action: #selector(didTapTermNoButton), for: .touchUpInside)
+    }
+    
+    let termView = UIView()
+    let termStartLabel = UILabel().then {
+        $0.text = "시작 날짜"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let termStartTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
+    let termEndLabel = UILabel().then {
+        $0.text = "종료 날짜"
+        $0.sizeToFit()
+        $0.font = Font.studyContentsLabel
+    }
+    let termEndTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.addBorder(.bottom, color: .signatureColor, thickness: 1.0)
+        //        $0.addTarget(self, action: #selector(LoginVC.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+    }
+    
     // MARK: - Variables and Properties
     
-    
+    var isClickedPenalty = false
+    var isClickedTerm = false
     
     // MARK: - Life Cycle
     
@@ -40,6 +171,74 @@ class AddStudyVC: UITableViewController {
     
     // MARK: - Helper
     
+    func updatePenaltyStatus() {
+        if isClickedPenalty == true {
+            penaltyYesButton.alpha = 1.0
+            penaltyNoButton.alpha = 0.5
+//            penaltyView.isHidden = false
+            
+            penaltyView.alpha = 1.0
+            penaltyView.isUserInteractionEnabled = true
+            
+//            penaltyView.snp.makeConstraints{ make in
+//                make.top.equalTo(penaltyYesButton.snp.bottom).offset(30)
+//            }
+//            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
+//                self.view.layoutIfNeeded()
+//            }, completion: nil)
+        } else {
+            penaltyYesButton.alpha = 0.5
+            penaltyNoButton.alpha = 1.0
+//            penaltyView.isHidden = true
+            
+            penaltyView.alpha = 0.5
+            penaltyView.isUserInteractionEnabled = false
+            
+//            penaltyView.snp.makeConstraints{ make in
+//                make.top.equalTo(penaltyYesButton.snp.bottom).offset(0)
+//                make.height.equalTo(0)
+//            }
+//            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
+//                self.view.layoutIfNeeded()
+//            }, completion: nil)
+        }
+    }
+    
+    func updateTermStatus() {
+        if isClickedTerm == true {
+            termYesButton.alpha = 1.0
+            termNoButton.alpha = 0.5
+            
+            termView.alpha = 1.0
+            termView.isUserInteractionEnabled = true
+        } else {
+            termYesButton.alpha = 0.5
+            termNoButton.alpha = 1.0
+            
+            termView.alpha = 0.5
+            termView.isUserInteractionEnabled = false
+        }
+    }
+    
+    @objc func didTapPenaltyYesButton() {
+        isClickedPenalty = true
+        updatePenaltyStatus()
+    }
+    
+    @objc func didTapPenaltyNoButton() {
+        isClickedPenalty = false
+        updatePenaltyStatus()
+    }
+    
+    @objc func didTapTermYesButton() {
+        isClickedTerm = true
+        updateTermStatus()
+    }
+    
+    @objc func didTapTermNoButton() {
+        isClickedTerm = false
+        updateTermStatus()
+    }
     
     
     
@@ -57,74 +256,80 @@ class AddStudyVC: UITableViewController {
     
     
     
-    
-    
-    
-    
-    
-    let category = ["카테고리","IT", "영어", "수학", "취업"]
-
-    @IBOutlet var studyNameTextField: UITextField!
-    @IBOutlet var categoryTextField: UITextField!
-    @IBOutlet var explanationTextView: UITextView!
-    @IBOutlet var startDateTextField: UITextField!
-    @IBOutlet var endDateTextField: UITextField!
-    @IBOutlet var headCountTextField: UITextField!
-    @IBOutlet var locationTextField: UITextField!
-    @IBOutlet var penaltyTextField: UITextField!
-    @IBOutlet var penaltyButton: UIButton!
-
-    let datePicker = UIDatePicker()
+//    let category = ["카테고리","IT", "영어", "수학", "취업"]
+//
+//    @IBOutlet var studyNameTextField: UITextField!
+//    @IBOutlet var categoryTextField: UITextField!
+//    @IBOutlet var explanationTextView: UITextView!
+//    @IBOutlet var startDateTextField: UITextField!
+//    @IBOutlet var endDateTextField: UITextField!
+//    @IBOutlet var headCountTextField: UITextField!
+//    @IBOutlet var locationTextField: UITextField!
+//    @IBOutlet var penaltyTextField: UITextField!
+//    @IBOutlet var penaltyButton: UIButton!
+//
+//    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        addSubView()
-        createPickerView()
-        
         title = "스터디"
-//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .done, target: self, action: #selector(didTapCancelButton))
-        
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "생성", style: .done, target: self, action: #selector(didTapCreateStudyButton))
+        
+        addSubView()
+        makeConstraints()
+        
+        updatePenaltyStatus()
+        updateTermStatus()
+        
+        containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapContainerView)))
+//        createPickerView()
+        
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .done, target: self, action: #selector(didTapCancelButton))
     }
     
-    private func createPickerView() {
-        // category picker
-        let categoryPickerView = UIPickerView()
-        categoryPickerView.delegate = self
-        categoryPickerView.dataSource = self
-        categoryTextField.inputView = categoryPickerView
-
-        // date picker
-        datePicker.datePickerMode = .date
-        startDateTextField.inputView = datePicker
-        endDateTextField.inputView = datePicker
-
-        // 공통 툴바
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        toolbar.tintColor = .signatureColor
-        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissPickerView))
-        toolbar.setItems([doneButton], animated: false)
-        toolbar.isUserInteractionEnabled = true
-        startDateTextField.inputAccessoryView = toolbar
-        endDateTextField.inputAccessoryView = toolbar
-        categoryTextField.inputAccessoryView = toolbar
-    }
-
-    @objc func dismissPickerView() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .full
-        let date = dateFormatter.string(from: datePicker.date)
-
-        if startDateTextField.isEditing {
-            startDateTextField.text = date
-
-        } else if endDateTextField.isEditing {
-            endDateTextField.text = date
-        }
-
-        view.endEditing(true)
+//    private func createPickerView() {
+//        // category picker
+//        let categoryPickerView = UIPickerView()
+//        categoryPickerView.delegate = self
+//        categoryPickerView.dataSource = self
+//        categoryTextField.inputView = categoryPickerView
+//
+//        // date picker
+//        datePicker.datePickerMode = .date
+//        startDateTextField.inputView = datePicker
+//        endDateTextField.inputView = datePicker
+//
+//        // 공통 툴바
+//        let toolbar = UIToolbar()
+//        toolbar.sizeToFit()
+//        toolbar.tintColor = .signatureColor
+//        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissPickerView))
+//        toolbar.setItems([doneButton], animated: false)
+//        toolbar.isUserInteractionEnabled = true
+//        startDateTextField.inputAccessoryView = toolbar
+//        endDateTextField.inputAccessoryView = toolbar
+//        categoryTextField.inputAccessoryView = toolbar
+//    }
+//
+//    @objc func dismissPickerView() {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateStyle = .full
+//        let date = dateFormatter.string(from: datePicker.date)
+//
+//        if startDateTextField.isEditing {
+//            startDateTextField.text = date
+//
+//        } else if endDateTextField.isEditing {
+//            endDateTextField.text = date
+//        }
+//
+//        view.endEditing(true)
+//    }
+    
+    @objc private func didTapContainerView() {
+        self.view.endEditing(true)
     }
     
     @objc private func didTapCancelButton() {
@@ -135,44 +340,44 @@ class AddStudyVC: UITableViewController {
 //        self.createStudyService()
     }
    
-    @IBAction func didTapPenaltyButton(_ sender: Any) {
-        if penaltyTextField.isEnabled  {
-            penaltyButton.setTitle("", for: .normal)
-            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
-            penaltyTextField.text = ""
-        } else {
-            penaltyButton.setTitle("✓", for: .normal)
-            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
-        }
-    }
+//    @IBAction func didTapPenaltyButton(_ sender: Any) {
+//        if penaltyTextField.isEnabled  {
+//            penaltyButton.setTitle("", for: .normal)
+//            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
+//            penaltyTextField.text = ""
+//        } else {
+//            penaltyButton.setTitle("✓", for: .normal)
+//            penaltyTextField.isEnabled = !penaltyTextField.isEnabled
+//        }
+//    }
     
     
 }
 
-extension AddStudyVC: UIPickerViewDelegate {}
-
-extension AddStudyVC: UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return category.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-
-        return category[row]
-    }
-
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if category[row] == "카테고리" {
-            return
-        }
-        categoryTextField.text = category[row]
-    }
-
-}
+//extension AddStudyVC: UIPickerViewDelegate {}
+//
+//extension AddStudyVC: UIPickerViewDataSource {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return category.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//
+//        return category[row]
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        if category[row] == "카테고리" {
+//            return
+//        }
+//        categoryTextField.text = category[row]
+//    }
+//
+//}
 
 // MARK: - 사용자 정보 서버 연결
 
--- skhustudy/Secene/Main/ViewController/AddStudyVC+Constraints.swift
+++ skhustudy/Secene/Main/ViewController/AddStudyVC+Constraints.swift
@@ -12,18 +12,67 @@ extension AddStudyVC {
     
     func addSubView() {
         
+        // Add SubView
         view.addSubview(scrollView)
         scrollView.addSubview(containerView)
+        
+        // 스터디 이미지 설정
         containerView.addSubview(studyImageView)
         studyImageView.addSubview(addStudyImageButton)
         
+        // 스터디 제목
+        containerView.addSubview(studyTitleLabel)
+        containerView.addSubview(studyTitleTextField)
+        
+        // 카테고리
+        containerView.addSubview(categoryLabel)
+        containerView.addSubview(categoryTextField)
+        
+        // 소개
+        containerView.addSubview(introduceLabel)
+        containerView.addSubview(introduceTextField)
+        
+        // 장소
+        containerView.addSubview(placeLabel)
+        containerView.addSubview(placeTextField)
+        
+        // 벌금 여부
+        containerView.addSubview(penaltyLabel)
+        containerView.addSubview(penaltyYesButton)
+        containerView.addSubview(penaltyNoButton)
+        
+        containerView.addSubview(penaltyView)
+        
+        penaltyView.addSubview(penaltyAttendanceLabel)
+        penaltyView.addSubview(penaltyAttendanceTextField)
+        
+        penaltyView.addSubview(penaltyLateLabel)
+        penaltyView.addSubview(penaltyLateTextField)
+        
+        // 모집 기간
+        containerView.addSubview(termLabel)
+        containerView.addSubview(termYesButton)
+        containerView.addSubview(termNoButton)
+        
+        containerView.addSubview(termView)
+        
+        termView.addSubview(termStartLabel)
+        termView.addSubview(termStartTextField)
+        
+        termView.addSubview(termEndLabel)
+        termView.addSubview(termEndTextField)
+        
+    }
+    
+    func makeConstraints() {
+        
+        // Add ScrollView with ContainerView
         scrollView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.left.equalToSuperview()
             make.right.equalToSuperview()
             make.bottom.equalToSuperview()
         }
-        
         containerView.snp.makeConstraints { make in
             make.top.equalTo(scrollView.snp.top)
             make.left.equalTo(scrollView.snp.left)
@@ -33,6 +82,9 @@ extension AddStudyVC {
             make.height.greaterThanOrEqualTo(scrollView.snp.height)
         }
         
+        
+        // Make Constraints
+        // 스터디 이미지 설정
         studyImageView.snp.makeConstraints { make in
             make.centerX.equalTo(containerView)
             make.top.equalTo(containerView.snp.top).offset(10)
@@ -44,6 +96,143 @@ extension AddStudyVC {
             make.bottom.equalTo(studyImageView.snp.bottom)
             make.width.equalTo(30)
             make.height.equalTo(addStudyImageButton.snp.width)
+        }
+        
+        
+        let betweenDifferentArea = 30
+        let betweenLabelAndTextField = 20
+        let betweenLeftAndRightContainerView = 25
+        let betweenButtons = 20
+        let buttonWidth = 80
+        // 스터디 제목
+        studyTitleLabel.snp.makeConstraints{ make in
+            make.top.equalTo(studyImageView.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        studyTitleTextField.snp.makeConstraints{ make in
+            make.top.equalTo(studyTitleLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+        }
+        
+        // 카테고리
+        categoryLabel.snp.makeConstraints{ make in
+            make.top.equalTo(studyTitleTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        categoryTextField.snp.makeConstraints{ make in
+            make.top.equalTo(categoryLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+        }
+        
+        // 소개
+        introduceLabel.snp.makeConstraints{ make in
+            make.top.equalTo(categoryTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        introduceTextField.snp.makeConstraints{ make in
+            make.top.equalTo(introduceLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+        }
+        
+        // 장소
+        placeLabel.snp.makeConstraints{ make in
+            make.top.equalTo(introduceTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        placeTextField.snp.makeConstraints{ make in
+            make.top.equalTo(placeLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+        }
+        
+        // 벌금 여부
+        penaltyLabel.snp.makeConstraints{ make in
+            make.top.equalTo(placeTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        penaltyYesButton.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.width.equalTo(buttonWidth)
+        }
+        penaltyNoButton.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(penaltyYesButton.snp.right).offset(betweenButtons)
+            make.width.equalTo(buttonWidth)
+        }
+        
+        penaltyView.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyYesButton.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+        }
+        
+        penaltyAttendanceLabel.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyView.snp.top)
+            make.left.equalTo(penaltyView.snp.left)
+        }
+        penaltyAttendanceTextField.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyAttendanceLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(penaltyView.snp.left)
+            make.right.equalTo(penaltyView.snp.right)
+        }
+        
+        penaltyLateLabel.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyAttendanceTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(penaltyView.snp.left)
+        }
+        penaltyLateTextField.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyLateLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(penaltyView.snp.left)
+            make.right.equalTo(penaltyView.snp.right)
+            make.bottom.equalTo(penaltyView.snp.bottom)
+        }
+        
+        // 모집 기간
+        termLabel.snp.makeConstraints{ make in
+            make.top.equalTo(penaltyView.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+        }
+        termYesButton.snp.makeConstraints{ make in
+            make.top.equalTo(termLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.width.equalTo(buttonWidth)
+        }
+        termNoButton.snp.makeConstraints{ make in
+            make.top.equalTo(termLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(termYesButton.snp.right).offset(betweenButtons)
+            make.width.equalTo(buttonWidth)
+        }
+        
+        termView.snp.makeConstraints{ make in
+            make.top.equalTo(termYesButton.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(containerView.snp.left).offset(betweenLeftAndRightContainerView)
+            make.right.equalTo(containerView.snp.right).inset(betweenLeftAndRightContainerView)
+            make.bottom.equalTo(containerView.snp.bottom).inset(30)
+        }
+        
+        termStartLabel.snp.makeConstraints{ make in
+            make.top.equalTo(termView.snp.top)
+            make.left.equalTo(termView.snp.left)
+        }
+        termStartTextField.snp.makeConstraints{ make in
+            make.top.equalTo(termStartLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(termView.snp.left)
+            make.right.equalTo(termView.snp.right)
+        }
+        
+        termEndLabel.snp.makeConstraints{ make in
+            make.top.equalTo(termStartTextField.snp.bottom).offset(betweenDifferentArea)
+            make.left.equalTo(termView.snp.left)
+        }
+        termEndTextField.snp.makeConstraints{ make in
+            make.top.equalTo(termEndLabel.snp.bottom).offset(betweenLabelAndTextField)
+            make.left.equalTo(termView.snp.left)
+            make.right.equalTo(termView.snp.right)
+            make.bottom.equalTo(termView.snp.bottom)
         }
     }
     

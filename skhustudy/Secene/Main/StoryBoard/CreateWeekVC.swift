b1f2651dfc9159f7c0aa579a9b3ad99bd26f5a30
Kim HeeJae <iowa329@gmail.com>
2020-06-13 01:41:39 +0900

    #65 날짜 입력 창 수정

--- skhustudy/Secene/Main/StoryBoard/CreateWeekVC.swift
+++ skhustudy/Secene/Main/StoryBoard/CreateWeekVC.swift
@@ -20,6 +20,7 @@ class CreateWeekVC: UIViewController {
     
     let datePicker = UIDatePicker().then {
         $0.datePickerMode = .date
+        $0.locale = Locale(identifier: "ko")
     }
     let dateFormatter = DateFormatter().then {
         $0.dateFormat = "yyyy-MM-dd"
@@ -94,10 +95,11 @@ class CreateWeekVC: UIViewController {
         title = "챕터 생성"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "생성", style: .done, target: self, action: #selector(didTapAddWeekButton))
         
-//        addKeyboardNotification()
-//        containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapContainerView)))
+        addKeyboardNotification()
+        containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapContainerView)))
         
-//        setDatePicker()
+        setDatePicker()
+        
         addSubView()
         makeConstraints()
     }
@@ -128,120 +130,119 @@ class CreateWeekVC: UIViewController {
     }
     
     // MARK: Methods
-//
-//    func addKeyboardNotification() {
-//         NotificationCenter.default.addObserver(
-//             self,
-//             selector: #selector(keyboardWillShow),
-//             name: UIResponder.keyboardWillShowNotification,
-//             object: nil)
-//
-//         NotificationCenter.default.addObserver(
-//             self,
-//             selector: #selector(keyboardWillHide),
-//             name: UIResponder.keyboardWillHideNotification,
-//             object: nil)
-//
-//     }
 
-//     @objc func keyboardWillShow(_ notification: Notification) {
-//        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
-//
-//         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
-//         scrollView.contentInset = contentInsets
-//         scrollView.scrollIndicatorInsets = contentInsets
-//
-//         var rect = self.view.frame
-//         rect.size.height -= keyboardFrame.height
-//         if rect.contains(memoTextView.frame.origin) {
-//             scrollView.scrollRectToVisible(memoTextView.frame, animated: true)
-//         }
-//     }
-//
-//     @objc func keyboardWillHide(_ notification: Notification) {
-//         let contentInsets = UIEdgeInsets.zero
-//         scrollView.contentInset = contentInsets
-//         scrollView.scrollIndicatorInsets = contentInsets
-//     }
-//
-//    private func setDatePicker() {
-//        dateTextField.inputView = datePicker
-//        let date = Date()
-//        let stringDate = dateFormatter.string(from: date)
-//        dateTextField.text = stringDate
-//
-//        let toolbar = UIToolbar()
-//        toolbar.sizeToFit()
-//        toolbar.tintColor = .signatureColor
-//        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissPickerView))
-//        toolbar.setItems([doneButton], animated: false)
-//        toolbar.isUserInteractionEnabled = true
-//
-//        dateTextField.inputAccessoryView = toolbar
-//
-//    }
-//
-//    @objc private func didTapContainerView() {
-//        self.view.endEditing(true)
-//    }
-//
-//
-//    @objc private func dismissPickerView() {
-//        let date = dateFormatter.string(from: datePicker.date)
-//        dateTextField.text = date
-//        view.endEditing(true)
-//    }
-//
+    func addKeyboardNotification() {
+         NotificationCenter.default.addObserver(
+             self,
+             selector: #selector(keyboardWillShow),
+             name: UIResponder.keyboardWillShowNotification,
+             object: nil)
+
+         NotificationCenter.default.addObserver(
+             self,
+             selector: #selector(keyboardWillHide),
+             name: UIResponder.keyboardWillHideNotification,
+             object: nil)
+
+     }
+
+     @objc func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+
+         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
+         scrollView.contentInset = contentInsets
+         scrollView.scrollIndicatorInsets = contentInsets
+
+         var rect = self.view.frame
+         rect.size.height -= keyboardFrame.height
+         if rect.contains(chapterContentTextView.frame.origin) {
+             scrollView.scrollRectToVisible(chapterContentTextView.frame, animated: true)
+         }
+     }
+
+     @objc func keyboardWillHide(_ notification: Notification) {
+         let contentInsets = UIEdgeInsets.zero
+         scrollView.contentInset = contentInsets
+         scrollView.scrollIndicatorInsets = contentInsets
+     }
+
+    private func setDatePicker() {
+        dateTextField.inputView = datePicker
+        let date = Date()
+        let stringDate = dateFormatter.string(from: date)
+        dateTextField.text = stringDate
+
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        toolbar.tintColor = .signatureColor
+        let doneButton = UIBarButtonItem(title: "확인", style: .plain, target: self, action: #selector(dismissPickerView))
+        toolbar.setItems([doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
+
+        dateTextField.inputAccessoryView = toolbar
+
+    }
+
+    @objc private func didTapContainerView() {
+        self.view.endEditing(true)
+    }
+
+    @objc private func dismissPickerView() {
+        let date = dateFormatter.string(from: datePicker.date)
+        dateTextField.text = date
+        view.endEditing(true)
+    }
+
     @objc private func didTapAddWeekButton() {
 //        createStudyChapterService(completionHandler: {
 //            self.navigationController?.popViewController(animated: true)
 //        })
     }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        view.endEditing(true)
-//    }
-//
-//
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+
 }
-//
-//// MARK: - Create Study Chapter Service
-//
-//extension CreateWeekVC {
-//
-//    func createStudyChapterService(completionHandler: @escaping () -> Void) {
-//        let token = KeychainWrapper.standard.string(forKey: "token") ?? ""
-//        StudyService.shared.createStudyChapter(token: token, id: studyID, content: subjectTextField.text ?? "", date: dateTextField.text ?? "", place: locationTextField.text ?? "") { result in
-//
-//            switch result {
-//                case .success(let res):
-//                    let responseResult = res as! Response
-//
-//                    switch responseResult.status {
-//                    case 200:
-//                        completionHandler()
-//
-//                    case 400, 406, 411, 500, 420, 421, 422, 423:
-//                        self.simpleAlert(title: responseResult.message, message: "")
-//
-//                        completionHandler()
-//
-//                    default:
-//                        self.simpleAlert(title: "오류가 발생하였습니다", message: "")
-//
-//                        completionHandler()
-//                    }
-//                case .requestErr(_):
-//                    print(".requestErr")
-//                case .pathErr:
-//                    print(".pathErr")
-//                case .serverErr:
-//                    print(".serverErr")
-//                case .networkFail:
-//                    print(".networkFail")
-//            }
-//
-//        }
-//    }
-//
-//}
+
+// MARK: - Create Study Chapter Service
+
+extension CreateWeekVC {
+
+    func createStudyChapterService(completionHandler: @escaping () -> Void) {
+        let token = KeychainWrapper.standard.string(forKey: "token") ?? ""
+        StudyService.shared.createStudyChapter(token: token, id: studyID, content: chapterContentTextView.text ?? "", date: dateTextField.text ?? "", place: placeTextField.text ?? "") { result in
+
+            switch result {
+                case .success(let res):
+                    let responseResult = res as! Response
+
+                    switch responseResult.status {
+                    case 200:
+                        completionHandler()
+
+                    case 400, 406, 411, 500, 420, 421, 422, 423:
+                        self.simpleAlert(title: responseResult.message, message: "")
+
+                        completionHandler()
+
+                    default:
+                        self.simpleAlert(title: "오류가 발생하였습니다", message: "")
+
+                        completionHandler()
+                    }
+                case .requestErr(_):
+                    print(".requestErr")
+                case .pathErr:
+                    print(".pathErr")
+                case .serverErr:
+                    print(".serverErr")
+                case .networkFail:
+                    print(".networkFail")
+            }
+
+        }
+    }
+
+}

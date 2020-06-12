128f5848ea0cf46680863996a34fcea88a48f1d2
Junhyeon <oh.junhyeon95@gmail.com>
2020-06-13 05:22:09 +0900

    스터디 검색 서버 연결 코드
    코드 추가

--- skhustudy/Global/Service/StudyService.swift
+++ skhustudy/Global/Service/StudyService.swift
@@ -330,4 +330,54 @@ struct StudyService {
             }
         }
     }
+    
+    // MARK: - Get Search List
+    
+    func getSearchList(_ token: String, _ name: String,completion: @escaping (NetworkResult<Any>) -> Void) {
+        
+        let URL = APIConstants.GetStudySearch + "?token=" + token + "&name=" + name
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        
+        AF.request(URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseData {
+            response in
+            
+            switch response.result {
+                
+            case .success:
+                if let value = response.value {
+                    if let status = response.response?.statusCode {
+                        switch status {
+                        case 200:
+                            do{
+                                let decoder = JSONDecoder()
+                                let result = try
+                                    decoder.decode(StudyList.self, from: value)
+                                completion(.success(result))
+                            } catch {
+                                
+                                completion(.pathErr)
+                            }
+                        case 409:
+                            
+                            completion(.pathErr)
+                        case 500:
+                            
+                            completion(.serverErr)
+                        default:
+                            break
+                        }
+                    }
+                }
+                break
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(.networkFail)
+            }
+        }
+    }
+
+
+    
 }

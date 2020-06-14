baa55759740b48fb3f713a7e9b71ae1dddaaa361
Kim HeeJae <iowa329@gmail.com>
2020-06-14 13:54:04 +0900

    #69 스터디 구성원 체크 관리 뷰 클래스 생성

---
+++ skhustudy/Secene/Main/Cell/CheckTVC.swift
@@ -0,0 +1,9 @@
+//
+//  CheckTVC.swift
+//  SKHUStudy
+//
+//  Created by Hee Jae Kim on 2020/06/14.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import Foundation
---
+++ skhustudy/Secene/Main/ViewController/CheckVC.swift
@@ -0,0 +1,9 @@
+//
+//  CheckVC.swift
+//  SKHUStudy
+//
+//  Created by Hee Jae Kim on 2020/06/14.
+//  Copyright © 2020 anniversary. All rights reserved.
+//
+
+import Foundation
